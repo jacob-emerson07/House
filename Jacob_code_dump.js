ecc133786d15d436db07293e7df03ea63417502b
Jacob Emerson <jacob.emerson07@gmail.com>
2019-02-09 15:59:52 -0800

    See "push-notes" channel

---
+++ Jacob_code_dump.js
@@ -0,0 +1,21 @@
+// Updated Shoot function for scatter shot
+// Meant to be placed in the Mover class
+/*
+  doShoot() {
+    if(win) return;
+    if(this.shooting&&this.shootTimer%10==0) {
+    for (i = 0; i < 5; i++){
+      var b = new Bullet(
+        this.x + Math.cos(this.aimAngle)*50,
+        this.y + Math.sin(this.aimAngle)*50,
+        this.aimAngle + ((i-2)*.1),this);
+      entities.push(b);
+      playerBullets.push(b);
+      SOUNDS.shoot.play();
+    }
+*/
+
+
+    
+    
+    
